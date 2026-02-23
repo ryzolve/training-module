73a6e99d539192c7279bad2bf8d57d66a6f2c905
Anurag Pal <apal895@gmail.com>
2026-02-23 21:58:51 +0530

    Update Tawk.to chat widget embed ID

--- src/components/chat-widget/ChatWidget.js
+++ src/components/chat-widget/ChatWidget.js
@@ -10,7 +10,7 @@ export default function ChatWidget() {
         (function(){
           var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
           s1.async=true;
-          s1.src='https://embed.tawk.to/69931c560c0e241c36362e84/1jhjadd64';
+          s1.src='https://embed.tawk.to/69922ad73a5ba51c3b88cf76/1jhhfemu5';
           s1.charset='UTF-8';
           s1.setAttribute('crossorigin','*');
           s0.parentNode.insertBefore(s1,s0);
